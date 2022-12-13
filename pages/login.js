26373a826a4d06a2d39de6efc74aa2802af64a51
Aaryanaditya Singh <99534166+Aaryan-47@users.noreply.github.com>
2022-12-13 16:20:03 +0530

    Update login.js

--- pages/login.js
+++ pages/login.js
@@ -9,6 +9,7 @@ function Login() {
     email: "",
     password: ""
   });
+  console.log(process.env.NODE_ENV)
 
   const { email, password } = user;
   const [showPassword, setShowPassword] = useState(false);
